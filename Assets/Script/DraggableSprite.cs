58bb8dbf6e2da9d7bac0875d27d7dd769812acf7
Siarhei Pilat <spilat12@gmail.com>
2020-04-18 08:02:39 +0200

    first

---
+++ Assets/Script/DraggableSprite.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableSprite : MonoBehaviour
+{
+    Vector3 pos;
+    Vector3 OriginalPos;
+    bool canDrag, canFeed;
+
+    public enum FoodType
+    {
+        ELECTRONICS,
+        BATTERIES
+    }
+
+    public FoodType foodType;
+
+    void Start()
+    {
+        OriginalPos = transform.position;
+    }
+
+    void OnMouseDrag()
+    {
+        switch (foodType)
+        {
+            case FoodType.ELECTRONICS:
+                canDrag = Manager.instance.Electronics > 0 ? true : false;
+                break;
+            case FoodType.BATTERIES:
+                canDrag = Manager.instance.Batteries > 0 ? true : false;
+                break;
+            default:
+                break;
+        }
+
+        if (canDrag)
+        {
+            Manager.instance.isDraggingUI = true;
+            pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(pos.x, pos.y, 0.0f); 
+        }
+    }
+
+    public void ReturnToPlace()
+    {
+        transform.position = OriginalPos;
+    }
+
+    // THIS DOESN'T WORK GOOD
+    void OnTriggerStay2D(Collider2D other)
+    {
+        canFeed = true;
+        //Debug.Log("entered");
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    FeedBoy();
+        //}
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        canFeed = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && canFeed)
+        {
+            FeedBoy();
+        }
+    }
+
+    void FeedBoy()
+    {
+        switch (foodType)
+        {
+            case FoodType.ELECTRONICS:
+                Manager.instance.BoyXP += 25;
+                break;
+            case FoodType.BATTERIES:
+                Manager.instance.AddEnergy(25.0f);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using NaughtyAttributes;
+using TMPro;
+using ScriptableObjectArchitecture;
+using UnityEngine.UI;
+
+// IDEA:
+// - Virus aesthetics
+// - Voiceover
+// - https://www.youtube.com/watch?v=nM2Wz6NdPZs
+
+// MVP:
+// - Depleting energy bar (done)
+// - Loot: energy source, electronics
+// - Day timer (done)
+// - Voice lines:
+//      "Do I look like a real boy, papa?"
+//      "Thank you, papa"
+//      "I must sleep... and grow, papa"
+//      "I want to be like you when I grow up, papa"
+//      "I must consume your soul... to become flesh and bone"
+// - Several levels of the "boy"
+// - Loot buttons
+// - Loot areas
+// - Some sort of inventory
+// - Drag and drop to feed the "boy" (done)
+
+public class Manager : MonoBehaviour
+{
+    public static Manager instance;
+
+    public FloatReference HourTickRate, EnergyDepleteRate;
+    public int Hour, Day;
+    public TextMeshProUGUI DayText, HourText;
+    public Slider EnergySlider;
+
+    // Inventory
+    public int Batteries, Electronics;
+    public DraggableSprite DraggableElectronics, DraggableBatteries;
+
+    // Control
+    public bool isDraggingUI;
+
+    // Boy Stats
+    public int BoyLevel, BoyXP;
+
+    void Start()
+    {
+        instance = this;
+        InvokeRepeating("HourTick", 3.0f, HourTickRate.Value);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDraggingUI = false;
+            DraggableElectronics.ReturnToPlace();
+            DraggableBatteries.ReturnToPlace();
+        }
+    }
+
+    void HourTick()
+    {
+        if(Hour + 1 == 24)
+        {
+            Hour = 0;
+            Day++;
+            CompleteDay();
+        }
+        else
+        {
+            Hour++;
+        }
+        
+        UpdateTimeUI();
+        DepleteEnergy();
+    }
+
+    void DepleteEnergy()
+    {
+        if(EnergySlider.value - EnergyDepleteRate.Value <= 0.0f)
+        {
+            EnergySlider.value = 0.0f;
+            GameOver();
+        }
+        else
+        {
+            EnergySlider.value -= EnergyDepleteRate.Value;
+        }
+    }
+
+    public void AddEnergy(float amount)
+    {
+        EnergySlider.value += amount;
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Boom");
+    }
+
+    void UpdateTimeUI()
+    {
+        DayText.text = "Day: " + Day.ToString();
+        HourText.text = "Hour: " + Hour.ToString();
+    }
+
+    void CompleteDay()
+    {
+
+    }
+}
